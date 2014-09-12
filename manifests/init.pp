cc68e1a738cc0a61c114aeb05efde8951321b092
unknown <adm.kjamrozek@HF0006566.nibdom.com.au>
2014-09-12 17:25:40 +1000

    require enterprise to set key

--- manifests/init.pp
+++ manifests/init.pp
@@ -171,9 +171,8 @@ class pbis (
   exec { 'set_license_key':
     path      => ['/opt/pbis/bin'],
     command   => "setkey-cli --key ${license_key}",
-    unless  => 'setkey-cli | /bin/grep -c key',
-    subscribe   => Package['pbis-enterprise'],
-    refreshonly => true
+    unless    => 'setkey-cli | /bin/grep -c key',
+    require   => Package['pbis-enterprise'],
   }
   
 }

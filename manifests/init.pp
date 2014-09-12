7981f630be722046a9ed1e630b53e43f6e53d203
unknown <adm.kjamrozek@HF0006566.nibdom.com.au>
2014-09-12 16:48:00 +1000

    no need to set key if set already

--- manifests/init.pp
+++ manifests/init.pp
@@ -171,6 +171,7 @@ class pbis (
   exec { 'set_license_key':
     path      => ['/opt/pbis/bin'],
     command   => "setkey-cli --key ${license_key}",
+    unless  => 'setkey-cli | grep -c key',
     subscribe   => Package['pbis-enterprise'],
     refreshonly => true
   }
